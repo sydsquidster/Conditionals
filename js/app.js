aa93841b8b2f8e418c7ee3151084bfdee31ae4df
sydsquidster <89406043+sydsquidster@users.noreply.github.com>
2021-09-19 16:03:04 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,23 +1,28 @@
-var ele = document.body.querySelector(".box");
-var eleAnswer = document.body.querySelector(".answer");
+var box = document.body.querySelector(".box");
+var answer = document.body.querySelector(".answer");
+var an = document.body.querySelector(".an");
 
-var question =prompt("What animal do I want?");
-
-// This is the conditional of my program
-
-if(question=="Seal"||question=="SEAL"||question=="seal"){
-    ele.innerHTML="You got the right animal, which was a seal!";
-}else if(question=="Monkey"||question=="MONKEY"||question=="monkey"){
-    ele.innerHTML = "You got the right animal, which was a monkey!";
-}else{
-    ele.innerHTML="Sorry the animal was a cute cuddly seal or a monkey!";
+var q1 = Number(prompt("Enter a number."));
+if(isNaN(q1)){
+    box.innerHTML = "Invalid first number." ;
 }
 
-var qTwo=(prompt("What is the first number?"));
-var qThree=(prompt("What is the second number?"));
-
-if(qTwo>3&&qThree<10){
-    eleAnswer.innerHTML = "The numbers are good.";
-}else{
-    eleAnswer.innerHTML="The numbers are bad.";
+var q2 = Number(prompt("Enter a second number."));
+if(isNaN(q2)){
+    answer.innerHTML = "Invalid second number." ;
 }
+
+
+var q3 = (prompt("Enter an operator (+  -  *  /)."));
+if(q3== "+"){
+    an.innerHTML = q1 + q2 ;
+} else if(q3=="-") {
+    an.innerHTML = q1 - q2;
+} else if (q3=="*"|| q3=="x" || q3=="X") {
+    an.innerHTML = q1 * q2;
+} else if(q3=="/") {
+    an.innerHTML = q1 / q2;
+} else{
+    an.innerHTML = "Invalid operator" ;
+}
+
